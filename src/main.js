494d00d125b11cd352f16a26304abeb108d4318d
smallbellows <given.sara@gmail.com>
2016-05-02 20:24:51 +0000

    implement jquery plugin heapbox to style the select dropdown

--- src/main.js
+++ src/main.js
@@ -1,95 +1,100 @@
 $(function() {
 
-    // When a section is chosen, the page will do the following:
-    // -change the class of the header to 'loaded'
-    // -display the loading .gif
-    // -use ajax to load in up to 12 articles of the chosen section
-    // -for each story loaded:
-    //    -create a div with a background image of the featured image
-    //    -create a paragraph with the blurb
-    //    -link the div to the url of the article
-    // -hide the loading .gif
+/*
+ INSTANEWS WEBSITE
+    When a section is chosen by the user:
+    -change the class of the header to 'loaded'
+    -display the loading .gif
+    -use ajax to load in up to 12 articles of the chosen section
+    -for each story loaded:
+        -create a div with a background image of the featured image
+        -create a paragraph with the blurb
+        -link the div to the url of the article
+    -hide the loading .gif
+  Select is styled with heapbox jquery plugin
+*/
+      $('select').heapbox( {
+        'onChange': function (input) {
 
-    $('select').on('change', function () {
+          var $section = $('section');
+          var $header = $('header');
 
-        var $section = $('section');
-        var $header = $('header');
+          // the header will shrink to it's smaller height 
+          $header.animate({
+              height: $header.css('height', 'auto').height(),
+              }, 'slow');
+          $header.addClass('loaded');
 
-        $header.animate({
-            height: $header.css('height', 'auto').height(),
-        }, 'slow');
-        $header.addClass('loaded');
+          $('.while-loading').show();
 
-        $('.while-loading').show();
+          $section.empty();
 
-        $section.empty();
+          var userSection = input;
+          var urlForAPI = 'http://api.nytimes.com/svc/topstories/v1/'
+                          + userSection
+                          + '.json?api-key=8b4edc1d68ed46052e25047d8cbb612a:15:75124067';
 
-        var userSection = $(this).val();
-        var urlForAPI = 'http://api.nytimes.com/svc/topstories/v1/'
-                        + userSection
-                        + '.json?api-key=8b4edc1d68ed46052e25047d8cbb612a:15:75124067';
+          $.ajax({
+                  method: 'GET',
+                  url: urlForAPI,
+                  dataType: 'json',
+              })
+              .done(function (data) {
 
-        $.ajax({
-                method: 'GET',
-                url: urlForAPI,
-                dataType: 'json',
-            })
-            .done(function (data) {
+                  // data comes back as an object, one property of which is called results
+                  // data.results is an array containing all the articles of the chosen section
 
-                // data comes back as an object, one property of which is called results
-                // data.results is an array containing all the articles of the chosen section
+                  var nytResults = data.results;
 
-                var nytResults = data.results;
-                if (nytResults.length === 0) {
+                  if (nytResults.length === 0) {
 
-                    $section.append('<p class=\'no-results\'>Sorry no results were found</p>');
+                      $section.append('<p class=\'no-results\'>Sorry no results were found</p>');
 
-                } else {
+                  } else {
 
-                    if (nytResults !== 0) {
+                      if (nytResults !== 0) {
 
-                        nytResults = nytResults.filter(function (item) {
-                            return item.multimedia.length;
-                        }).splice(0, 12);
-                    }
+                          nytResults = nytResults.filter(function (item) {
+                              return item.multimedia.length;
+                          }).splice(0, 12);
+                      }
 
-                    //nytResults[] now holds max 12 articles that all have a photo attached
+                      //nytResults[] now holds max 12 articles that all have a photo attached
 
-                    var articlesToAppend = '';
-                    $.each(nytResults, function (index, value) {
+                      var articlesToAppend = '';
+                      $.each(nytResults, function (index, value) {
 
-                        var imgUrl = '';
-                        $.each(value.multimedia, function (key, val) {
+                          var imgUrl = '';
+                          $.each(value.multimedia, function (key, val) {
 
-                            if (val.format === 'superJumbo') {
-                                imgUrl = val.url;
-                            }
-                        });
+                              if (val.format === 'superJumbo') {
+                                  imgUrl = val.url;
+                              }
+                          });
 
-                        articlesToAppend += '<article>'
-                                            + '<a href=\'' + value.url + '\' target=\'_blank\'>'
-                                            + '<div class= \'inner\''
-                                            + ' style = "background: url(\'' + imgUrl + '\');'
-                                            + 'background-size: cover">'
-                                            + '<p>' + value.abstract + '</p></div></a>'
-                                          + '</article>';
-                    });
+                          articlesToAppend += '<article>'
+                                              + '<a href=\'' + value.url + '\' target=\'_blank\'>'
+                                              + '<div class= \'inner\''
+                                              + ' style = "background: url(\'' + imgUrl + '\');'
+                                              + 'background-size: cover">'
+                                              + '<p>' + value.abstract + '</p></div></a>'
+                                            + '</article>';
+                      });
 
-                    $section.append(articlesToAppend);
+                      $section.append(articlesToAppend);
 
-                    // when user hovers over an image, the abstract text appears
+                      // when user hovers over an image, the abstract text appears
 
-                    $('.inner').hover(function () {
-                        $(this).children().slideToggle(1000);
-                    });
+                      $('.inner').hover(function () {
+                          $(this).children().slideToggle(1000);
+                      });
 
-                }
+                  }
 
-            })
-            .always(function () {
-                $('.while-loading').hide();
-            });
-
-    });
-
+              })
+              .always(function () {
+                  $('.while-loading').hide();
+              });
+        }
+      });
 });
